3eb9732354e54733292b0222cd1bf18ad733f603
William Wu <william.wu@rock-chips.com>
2018-04-13 09:46:29 +0800

    phy: rockchip-inno-usb2: open pre-emphasize for rk322x
    Open pre-emphasize in non-chirp state for rk322x USB
    PHY0 otg port to increase HS slew rate.
    
    Signed-off-by: William Wu <william.wu@rock-chips.com>

--- drivers/phy/phy-rockchip-inno-usb2.c
+++ drivers/phy/phy-rockchip-inno-usb2.c
@@ -1618,6 +1618,17 @@ disable_clks:
 	return ret;
 }
 
+static int rk322x_usb2phy_tuning(struct rockchip_usb2phy *rphy)
+{
+	int ret = 0;
+
+	/* Open pre-emphasize in non-chirp state for PHY0 otg port */
+	if (rphy->phy_cfg->reg == 0x760)
+		ret = regmap_write(rphy->grf, 0x76c, 0x00070004);
+
+	return ret;
+}
+
 static int rk3328_usb2phy_tuning(struct rockchip_usb2phy *rphy)
 {
 	int ret;
@@ -1811,6 +1822,7 @@ static const struct rockchip_usb2phy_cfg rk322x_phy_cfgs[] = {
 	{
 		.reg = 0x760,
 		.num_ports	= 2,
+		.phy_tuning	= rk322x_usb2phy_tuning,
 		.clkout_ctl	= { 0x0768, 4, 4, 1, 0 },
 		.port_cfgs	= {
 			[USB2PHY_PORT_OTG] = {
